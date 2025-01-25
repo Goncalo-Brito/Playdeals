184088207b5bd82540b2eaae21a469d173f844ea
Goncalo-Brito <teixeiragbrito@gmail.com>
2025-01-25 07:55:50 +0000

    javadock

--- controllers/giftcardController.js
+++ controllers/giftcardController.js
@@ -1,5 +1,18 @@
 const GiftCard = require("../models/GiftCard");
 
+/**
+ * Get all gift cards.
+ * 
+ * Fetches all gift cards from the database and returns them in the response.
+ * In case of an error, a 500 status is returned with an error message.
+ * 
+ * @function getAll
+ * @async
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Sends a response with a list of gift cards or an error message.
+ */
 exports.getAll = async (req, res, next) => {
     try {
         const [GiftCards, _] = await GiftCard.getAll();
@@ -8,9 +21,21 @@ exports.getAll = async (req, res, next) => {
         console.log(error);
         res.status(500).json({ message: "An error occurred", error: error.message });
     }
-}
+};
 
-exports.getById = async (req, res, next) => {
+/**
+ * Get gift card info and render the card page.
+ * 
+ * Fetches a specific gift card by ID and renders the 'cardpage' view.
+ * 
+ * @function getCardPage
+ * @async
+ * @param {Object} req - The request object containing the gift card ID.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Renders the 'cardpage' view with gift card info.
+ */
+exports.getCardPage = async (req, res, next) => {
     try {
         const [[cardsinfo, _]] = await GiftCard.getById(req.params.id);
         res.render("cardpage", { cardsinfo });
@@ -18,9 +43,21 @@ exports.getById = async (req, res, next) => {
         console.log(error);
         next(error);
     }
-}
+};
 
-exports.getById = async (req, res, next) => {
+/**
+ * Get gift card info and render the cart page.
+ * 
+ * Fetches a specific gift card by ID and renders the 'cartpage' view.
+ * 
+ * @function getCartPage
+ * @async
+ * @param {Object} req - The request object containing the gift card ID.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Renders the 'cartpage' view with gift card info.
+ */
+exports.getCartPage = async (req, res, next) => {
     try {
         const [[cardsinfo, _]] = await GiftCard.getById(req.params.id);
         res.render("cartpage", { cardsinfo });
@@ -28,9 +65,21 @@ exports.getById = async (req, res, next) => {
         console.log(error);
         next(error);
     }
-}
+};
 
-exports.getById = async (req, res, next) => {
+/**
+ * Get gift card info and render the profile page.
+ * 
+ * Fetches a specific gift card by ID and renders the 'profilepage' view.
+ * 
+ * @function getProfilePage
+ * @async
+ * @param {Object} req - The request object containing the gift card ID.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Renders the 'profilepage' view with gift card info.
+ */
+exports.getProfilePage = async (req, res, next) => {
     try {
         const [[cardsinfo, _]] = await GiftCard.getById(req.params.id);
         res.render("profilepage", { cardsinfo });
@@ -38,9 +87,21 @@ exports.getById = async (req, res, next) => {
         console.log(error);
         next(error);
     }
-}
+};
 
-exports.getById = async (req, res, next) => {
+/**
+ * Get gift card info and render the payment page.
+ * 
+ * Fetches a specific gift card by ID and renders the 'payment' view.
+ * 
+ * @function getPaymentPage
+ * @async
+ * @param {Object} req - The request object containing the gift card ID.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Renders the 'payment' view with gift card info.
+ */
+exports.getPaymentPage = async (req, res, next) => {
     try {
         const [[cardsinfo, _]] = await GiftCard.getById(req.params.id);
         res.render("payment", { cardsinfo });
--- controllers/profilepictureController.js
+++ controllers/profilepictureController.js
@@ -1,5 +1,17 @@
 const ProfilePicture = require("../models/ProfilePicture");
 
+/**
+ * Get all profile pictures and render the staff page.
+ * 
+ * Fetches all profile pictures from the database and renders the 'staffpage' view.
+ * 
+ * @function getAll
+ * @async
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Renders the 'staffpage' view with profile pictures.
+ */
 exports.getAll = async (req, res, next) => {
     try {
         const [ProfilePictures, _] = await ProfilePicture.getAll();
@@ -8,9 +20,21 @@ exports.getAll = async (req, res, next) => {
         console.log(error);
         next(error);
     }
-}
+};
 
-exports.getById = async (req, res, next) => {
+/**
+ * Get profile picture info and render the profile page.
+ * 
+ * Fetches a specific profile picture by ID and renders the 'profilepage' view.
+ * 
+ * @function getProfilePage
+ * @async
+ * @param {Object} req - The request object containing the profile picture ID.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Renders the 'profilepage' view with profile picture info.
+ */
+exports.getProfilePage = async (req, res, next) => {
     try {
         const [[profilepicture, _]] = await ProfilePicture.getById(req.params.id);
         res.render("profilepage", { profilepicture });
@@ -18,9 +42,21 @@ exports.getById = async (req, res, next) => {
         console.log(error);
         next(error);
     }
-}
+};
 
-exports.getById = async (req, res, next) => {
+/**
+ * Get profile picture info and render the settings page.
+ * 
+ * Fetches a specific profile picture by ID and renders the 'settings' view.
+ * 
+ * @function getSettingsPage
+ * @async
+ * @param {Object} req - The request object containing the profile picture ID.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Renders the 'settings' view with profile picture info.
+ */
+exports.getSettingsPage = async (req, res, next) => {
     try {
         const [[profilepicture, _]] = await ProfilePicture.getById(req.params.id);
         res.render("settings", { profilepicture });
@@ -28,9 +64,21 @@ exports.getById = async (req, res, next) => {
         console.log(error);
         next(error);
     }
-}
+};
 
-exports.getById = async (req, res, next) => {
+/**
+ * Get profile picture info and render the auctions page.
+ * 
+ * Fetches a specific profile picture by ID and renders the 'auctionspage' view.
+ * 
+ * @function getAuctionsPage
+ * @async
+ * @param {Object} req - The request object containing the profile picture ID.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Renders the 'auctionspage' view with profile picture info.
+ */
+exports.getAuctionsPage = async (req, res, next) => {
     try {
         const [[profilepicture, _]] = await ProfilePicture.getById(req.params.id);
         res.render("auctionspage", { profilepicture });
@@ -38,36 +86,77 @@ exports.getById = async (req, res, next) => {
         console.log(error);
         next(error);
     }
-}
+};
 
+/**
+ * Create a new profile picture.
+ * 
+ * Creates a new profile picture using the provided data and saves it to the database.
+ * 
+ * @function create
+ * @async
+ * @param {Object} req - The request object containing the profile picture data.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Sends a response indicating the profile picture was created.
+ */
 exports.create = async (req, res, next) => {
     let { pfpextention, pfpsource, pfpname, gameID } = req.body;
 
-    let profilepicture = new profilepicture(pfpextention, pfpsource, pfpname, gameID);
+    let profilepicture = new ProfilePicture(pfpextention, pfpsource, pfpname, gameID);
 
-    profilepicture = await ProfilePicture.create();
+    try {
+        await profilepicture.create();
+        res.send("Profile Picture created: " + profilepicture);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error creating Profile Picture.");
+    }
+};
 
-    res.send("Profile Picture created: " + profilepicture);
-}
-
+/**
+ * Update a profile picture by its ID.
+ * 
+ * Updates a profile picture's data by ID with the provided updated data.
+ * 
+ * @function updateById
+ * @async
+ * @param {Object} req - The request object containing the profile picture ID and updated data.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Sends a response indicating the profile picture was updated.
+ */
 exports.updateById = async (req, res, next) => {
     try {
         const { id } = req.params;
         const updatedData = req.body;
 
         const [result] = await ProfilePicture.updateById(id, updatedData);
-        res.send("Profile Picture updated: " + profilepicture);
+        res.send("Profile Picture updated: " + result);
     } catch (error) {
         console.error(error);
         next(error);
     }
 };
 
+/**
+ * Delete a profile picture by its ID.
+ * 
+ * Deletes a profile picture from the database by its ID.
+ * 
+ * @function deleteById
+ * @async
+ * @param {Object} req - The request object containing the profile picture ID.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} - Sends a response indicating the profile picture was deleted.
+ */
 exports.deleteById = async (req, res, next) => {
     try {
         const { id } = req.params;
 
         const [result] = await ProfilePicture.deleteById(id);
+        res.send("Profile Picture deleted.");
     } catch (error) {
         console.error(error);
         next(error);
